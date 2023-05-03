1b5a17d2be4fcd17a97c9fe26f1a9531c3611baa
JesusFong23 <132445660+JesusFong23@users.noreply.github.com>
2023-05-03 04:56:27 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ image = Image.open('Green and Violet Y2k Party Stickers Dating Static Snapchat S
 
 st.image(image, caption='Prototype')
 
-conn = st.experimental_connection("sql")
+conn = st.experimental_connection("db-1copy.sql")
 n = st.slider("Pick a number")
 if st.button("Add the number!"):
     with conn.session as session:
