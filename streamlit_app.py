ec9b83df31f351d6e950eb68a56ed5b3c48f9e61
JesusFong23 <132445660+JesusFong23@users.noreply.github.com>
2023-05-03 04:59:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,6 @@ image = Image.open('Green and Violet Y2k Party Stickers Dating Static Snapchat S
 
 st.image(image, caption='Prototype')
 
-conn = st.experimental_connection("db-1copy.sql")
-n = st.slider("Pick a number")
-if st.button("Add the number!"):
-    with conn.session as session:
-        session.execute("INSERT INTO numbers (val) VALUES (:n);", {"n": n})
-        session.commit()
+conn = st.experimental_connection("databasedating.sql")
+df = conn.query("select * from FIRST_NAME ")
+st.dataframe(df)
