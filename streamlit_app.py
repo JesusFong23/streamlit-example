cab93606daa67659a1791f815e333329f06e1b3c
JesusFong23 <132445660+JesusFong23@users.noreply.github.com>
2023-05-03 04:55:32 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,3 +15,10 @@ and create a brew-tiful relationship :heart:
 image = Image.open('Green and Violet Y2k Party Stickers Dating Static Snapchat Snap Ad.jpg')
 
 st.image(image, caption='Prototype')
+
+conn = st.experimental_connection("sql")
+n = st.slider("Pick a number")
+if st.button("Add the number!"):
+    with conn.session as session:
+        session.execute("INSERT INTO numbers (val) VALUES (:n);", {"n": n})
+        session.commit()
