b8ecb21b08a539e88c7b39d6511a9a31ff6a9e6f
JesusFong23 <132445660+JesusFong23@users.noreply.github.com>
2023-05-03 04:13:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+from PIL import Image
 
 """
 # Welcome to Connection Cafe!
@@ -11,3 +12,6 @@ Where you can matcha with someone ;)
 
 and create a brew-tiful relationship <3
 """
+image = Image.open('sunrise.jpg')
+
+st.image(image, caption='Sunrise by the mountains')
