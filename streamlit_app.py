3bd63b8083dc6566e17f85905f4e2390b5e47abb
JesusFong23 <132445660+JesusFong23@users.noreply.github.com>
2023-05-03 03:59:13 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# Bienvenido a Connection Cafe!
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
